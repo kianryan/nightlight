bca01e0a085f422d33862749f144b17d15306b9f
Kian Ryan <kian@orangetentacle.co.uk>
2019-10-06 14:19:10 +0100

    Use FastLED
    Remove discrete LEDs, use strip of WS2812s with FastLED library.

--- nightlight.ino
+++ nightlight.ino
@@ -1,75 +1,95 @@
 #include <CapacitiveSensor.h>
-#include <EEPROM.h>
- 
-int light_pins[] = {8, 7, 6, 5};
+#include <FastLED.h>
+#include <EEPROMex.h>
+
+#define DEBUG       false
+
+#define LIGHT_PIN   6
+#define NUM_LEDS    4
+CRGB leds[NUM_LEDS];
+
+#define NUM_SENSORS 3
 CapacitiveSensor sensors[] = {
-  CapacitiveSensor(A4, A0), 
-  CapacitiveSensor(A4, A1), 
-  CapacitiveSensor(A4, A2), 
-  CapacitiveSensor(A4, A3) };
+  CapacitiveSensor(5, 0),
+  CapacitiveSensor(5, 1),
+  CapacitiveSensor(5, 2),
+  };
 
-int colours[] = {0, 0, 0, 0};
-bool dir[] = {false, false, false, false};
+int colours[] = {0, 0, 0};
+bool dir[] = {false, false, false};
+bool writeToRom = false;
 
-void setup()                    
+void setup()
 {
-  //Serial.begin(9600);
+  if (DEBUG) {
+    Serial.begin(9600);
+  }
+
+  FastLED.addLeds<WS2812, LIGHT_PIN, GRB>(leds, NUM_LEDS);
 
   loadEEPROM();
-
-  // Init LED lights
-  for (int i=0; i < 5; i++) {
-    pinMode(light_pins[i], OUTPUT);
-    analogWrite(light_pins[i], colours[i]);
-  }
 }
 
-void loop()                    
+void loop()
 {
+  writeToRom = false;
+  
   // Read each sensor.  If sensor is over 100, increase channel and write pin.
-  for(int i = 0; i < 4; i++) {
+  for(int i = 0; i < NUM_LEDS; i++) {
     long total = sensors[i].capacitiveSensor(30);
+
     if (total > 100) {
       if (! dir[i]) { // Increasing dir
+        writeToRom = true;
         colours[i] += 5;
         if (colours[i] == 255) {
-          dir[i] = true;  
-        }  
+          dir[i] = true;
+        }
       } else { // Decreasing dir
+        writeToRom = true;
         colours[i] -= 5;
         if (colours[i] == 0) {
-          dir[i] = false;  
-        } 
+          dir[i] = false;
+        }
       }
-      
-      analogWrite(light_pins[i], colours[i]); 
     }
   }
+  
+  /* Write colour to string to leds */
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(colours[0], colours[1], colours[2]); // Create colour from RGB
+  }
+  FastLED.show();
 
-  updateEEPROM();
+  /* Only write to rom if a change occurs */
+  if (writeToRom) {
+    updateEEPROM();
+  }
 
-  // char tbs[32];
-  // sprintf(tbs, "%d\t%d\t%d\t%d",colours[0],colours[1],colours[2],colours[3]);
-  //Serial.println(tbs);
+  if (DEBUG) {
+    char tbs[32];
+    sprintf(tbs, "%d\t%d\t%d\t%d",colours[0],colours[1],colours[2],colours[3]);
+    Serial.println(tbs);
+  }
   delay(50);
 }
 
 /* Load data from EEPROM */
 void loadEEPROM()
 {
+  byte temp_dir;
   int address = 0;
-  for (int i = 0; i < 4; i++) {
-    colours[i] = (int)EEPROM.read(i);
-    dir[i] = (bool)EEPROM.read(i+4); 
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    colours[i] = EEPROM.readInt(i);            // Read colour
+    dir[i] = EEPROM.readBit(NUM_SENSORS, i);   // Read direction
   }
 }
 
 /* Write data to EEPROM */
 void updateEEPROM()
 {
-  for (int i = 0; i < 4; i++) {
-    EEPROM.update(i, colours[i]);
-    EEPROM.update(i+4, dir[i]);  
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    EEPROM.updateInt(i, colours[i]);            // Write colour
+    EEPROM.updateBit(NUM_SENSORS, i, dir[i]);   // Write direction
   }
 }
-
